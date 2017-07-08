41657e2cd9fd356372b7bc84cbb4259e4efe10cb
Abhijit Kalta <abhijitkalta@gmail.com>
2017-07-08 08:05:52 +0530

    mongodb queries added

---
+++ database/queries/GetYearsActiveRange.js
@@ -0,0 +1,34 @@
+const Artist = require('../models/artist');
+
+/**
+ * Finds the lowest and highest yearsActive of artists in the Artist collection
+ * @return {promise} A promise that resolves with an object
+ * containing the min and max yearsActive, like { min: 0, max: 14 }.
+ */
+module.exports = () => {
+  const minQuery = Artist.find({})
+    .sort({
+      yearsActive: 1
+    })
+    .limit(1)
+    .then((artists) => {
+      return artists[0].yearsActive;
+    });
+
+  const maxQuery = Artist.find({})
+    .sort({
+      yearsActive: -1
+    })
+    .limit(1)
+    .then((artists) => {
+      return artists[0].yearsActive;
+    });
+
+  return Promise.all([minQuery, maxQuery])
+    .then((value) => {
+      return {
+        min: value[0],
+        max: value[1]
+      }
+    });
+};
